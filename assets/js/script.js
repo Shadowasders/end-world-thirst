54abd74f67a14c6aa11c95f320af92b6d4d5f4c8
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-25 21:20:31 -0400

    create two functions in the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,30 @@
 const btnSubmit = document.querySelector("#submit-btn");
 const userZip = document.querySelector("#zipsubmit");
 
+function init(){
+    let tempVal = localStorage.getItem();
+};
+
 btnSubmit.addEventListener("click", function(){
     console.log("Hi");
+    let tempUserVal;
     console.log(userZip.value);
+    tempUserVal = parseInt(userZip.value);
+    console.log(tempUserVal);
+    if (isNaN(tempUserVal)) {
+        renderInvalidMessage();
+    }
+    else {
+        console.log("valid input");
+    }
+    let userInput = localStorage.setItem("input", userZip.value);
+
 })
+
+function renderInvalidMessage(){
+    let warningMessage;
+    warningMessage = document.createElement("p");
+    warningMessage.style.color = "red";
+    warningMessage.textContent = "invalid input, L + ratio + you suck"
+    btnSubmit.parentElement.appendChild(warningMessage);
+}
